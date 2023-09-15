6b707d048dd8f8040466e1a5736620d36e1149f9
asxisx <102982608+asxisx@users.noreply.github.com>
2023-09-15 19:53:34 +0530

    Update powershell-sharepoint.ps1

--- powershell-sharepoint.ps1
+++ powershell-sharepoint.ps1
@@ -4,3 +4,27 @@ $username = "admin@contoso.sharepoint.com"
 $password = "password" 
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -argumentlist $userName, $(convertto-securestring $Password -asplaintext -force) 
 Connect-SPOService -Url https://contoso-admin.sharepoint.com -Credential $cred
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Connect with PnP
+
+Install-Module PnP.Powershell
+
+# Then create a certificate and upload it to your app registration. Get the thumbprint.
+# to know how to create a certificate, check another file powershell-certificate something.
+
+$SiteURL = ""
+$ClientID = ""
+$ThumbPrint = ""
+$Tenant = ""
+ 
+#Connect to SharePoint Online using Certificate
+Connect-PnPOnline -Url $SiteURL -ClientId $ClientID -Thumbprint $ThumbPrint -Tenant $Tenant
+ 
+#Get the Site
+Get-PnPSite
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
