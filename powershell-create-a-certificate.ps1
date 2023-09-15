55ff8e117f0d9a2dc835c632200db960cd433f05
asxisx <102982608+asxisx@users.noreply.github.com>
2023-09-15 18:44:54 +0530

    Create powershell-create-a-certificate.ps1

---
+++ powershell-create-a-certificate.ps1
@@ -0,0 +1,14 @@
+$CertificateName = "SharePoint Online Certificate"
+$CertificatePassword = "Password1"
+ 
+#Get the "Documents" folder
+$DocumentsFolder = [Environment]::GetFolderPath("MyDocuments")
+ 
+#Generate a Self-signed Certificate
+$Certificate = New-SelfSignedCertificate -Subject $CertificateName -CertStoreLocation "Cert:\CurrentUser\My" -KeyExportPolicy Exportable -KeySpec Signature -KeyLength 2048 -KeyAlgorithm RSA -HashAlgorithm SHA256
+ 
+#Export the Certificate to "Documents" Folder in your computer
+Export-Certificate -Cert $Certificate -FilePath $DocumentsFolder\$CertificateName.cer
+ 
+#Export the PFX File
+Export-PfxCertificate -Cert $Certificate -FilePath "$DocumentsFolder\$CertificateName.pfx" -Password (ConvertTo-SecureString -String $CertificatePassword -Force -AsPlainText)
